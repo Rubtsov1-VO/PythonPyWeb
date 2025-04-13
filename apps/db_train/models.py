2c8968678f8ca7b1a68f6a45a842e29c12a9feb8
vladimirrubtsov <Vladimir08!>
2025-04-13 13:08:32 +0300

    2 lab is done

--- apps/db_train/models.py
+++ apps/db_train/models.py
@@ -117,3 +117,28 @@ class Author(models.Model):
 class Meta:
     verbose_name = "Автор"
     verbose_name_plural = "Авторы"
+
+class AuthorProfile(models.Model):
+    author = models.OneToOneField('Author', on_delete=models.CASCADE)
+    stage = models.IntegerField(default=0,
+                                blank=True,
+                                verbose_name="Стаж",
+                                help_text="Стаж в годах")
+    def __str__(self):
+        return f"Автор: {self.author.username}; Стаж: {self.stage} лет"
+
+class Entry(models.Model):
+    text = models.TextField(verbose_name="Текст статьи",
+                            )
+    tags = models.ManyToManyField("Tag", related_name='entries')
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name='entries')
+
+    def __str__(self):
+        return f"Автор: {self.author.username}; Текст: {self.text}; Тэг: {self.tags.name}"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name="Название",
+                            )
+    def __str__(self):
+        return f"Тэг: {self.name}"
